68cf843d8b85292be936ec7870747f9c84efaa90
Holger Weiß <holger@weiss.in-berlin.de>
2008-01-06 03:24:00 +0000

    Add various new features and bug fixes
    Add lots of new features, fix many bugs, and incorporate various
    improvements done by Andrew Tridgell, Russ Allbery, Hrvoje Niksic,
    Damien Miller, and others for the Samba, INN, Wget, and OpenSSH
    projects.  The additions include: support the "e", "E", "g", "G", and
    "F" conversion specifiers (and use conversion style "f" or "F" for the
    still unsupported "a" and "A" specifiers); support the "hh", "ll", "j",
    "t", and "z" length modifiers; support the "#" flag and the (non-C99)
    "'" flag; use localeconv(3) (if available) to get both the current
    locale's decimal point character and the separator between groups of
    digits; fix the handling of various corner cases of field width and
    precision specifications; fix various floating point conversion bugs;
    handle infinite and NaN floating point values; don't attempt to write to
    the output buffer (which may be NULL) if a size of zero was specified;
    check for integer overflow of the field width, precision, and return
    values and during the floating point conversion; use the OUTCHAR() macro
    instead of a function for better performance; provide asprintf(3) and
    vasprintf(3) functions; add new test cases.  The replacement functions
    have been renamed to use an "rpl_" prefix, the function calls in the
    main project (and in this file) must be redefined accordingly for each
    replacement function which is needed (by using Autoconf or other means).
    Various other minor improvements have been applied and the coding style
    was cleaned up for consistency.

---
+++ system.h
@@ -0,0 +1,25 @@
+/* $Id$ */
+
+#ifndef SYSTEM_H
+#define SYSTEM_H
+
+#if HAVE_CONFIG_H
+#include <config.h>
+#endif	/* HAVE_CONFIG_H */
+
+#if HAVE_STDARG_H
+#include <stdarg.h>
+#if !HAVE_VSNPRINTF
+int rpl_vsnprintf(char *, size_t, const char *, va_list);
+#endif	/* !HAVE_VSNPRINTF */
+#if !HAVE_SNPRINTF
+int rpl_snprintf(char *, size_t, const char *, ...);
+#endif	/* !HAVE_SNPRINTF */
+#if !HAVE_VASPRINTF
+int rpl_vasprintf(char **, const char *, va_list);
+#endif	/* !HAVE_VASPRINTF */
+#if !HAVE_ASPRINTF
+int rpl_asprintf(char **, const char *, ...);
+#endif	/* !HAVE_ASPRINTF */
+#endif	/* HAVE_STDARG_H */
+#endif	/* SYSTEM_H */
---
+++ test.sh
@@ -0,0 +1,20 @@
+#!/bin/sh
+#
+# $Id$
+
+test -n "$CC" || \
+CC="gcc"
+
+test -n "$CPPFLAGS" || \
+CPPFLAGS="-DTEST_SNPRINTF"
+
+test -n "$LIBS" || \
+LIBS="-lm"
+
+test -n "$CFLAGS" || \
+CFLAGS="-g3 -O2 -Wall -W -Wbad-function-cast -Wcast-align -Wmissing-noreturn \
+	-Wnested-externs -Wpointer-arith -Wsign-compare -Wwrite-strings \
+	-Werror -std=c89 -pedantic"
+
+$CC $CFLAGS $CPPFLAGS -o snprintf snprintf.c $LIBS && \
+./snprintf
