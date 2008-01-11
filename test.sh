e756f58d2c3510e0208c3601e14a075bfbbfa006
Holger Weiß <holger@weiss.in-berlin.de>
2008-01-11 20:00:40 +0000

    Don't force strict C89 mode
    Don't force strict C89 mode, as we want to test "long long" support, for
    example.

--- test.sh
+++ test.sh
@@ -14,7 +14,7 @@ LIBS="-lm"
 test -n "$CFLAGS" || \
 CFLAGS="-g3 -O2 -Wall -W -Wbad-function-cast -Wcast-align -Wmissing-noreturn \
 	-Wnested-externs -Wpointer-arith -Wsign-compare -Wwrite-strings \
-	-Werror -std=c89 -pedantic"
+	-Werror"
 
 $CC $CFLAGS $CPPFLAGS -o snprintf snprintf.c $LIBS && \
 ./snprintf
